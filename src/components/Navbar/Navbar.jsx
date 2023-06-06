4b8911c8c795b2a403f2b86dcaf817a03a6d81af
Flu3nt <gnedyalkov94@gmail.com>
2023-06-06 14:39:54 +0300

    Refactor: Navbar now has logo and responsive design

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,6 +5,10 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
+      <Link to="/">
+        <h1 id="logo">G.</h1>
+      </Link>
+
       <ul className="navbar__items">
         <li>
           <Link to="/">Home</Link>
