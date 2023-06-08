7d1f5a800046aaa6426aeace496ad303bc3641dd
Flu3nt <gnedyalkov94@gmail.com>
2023-06-08 14:29:08 +0300

    Refactor: change the style of the form and added functionality to receive emails

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -20,39 +20,50 @@ const Contact = () => {
 
   return (
     <section id="contact-form">
-      <div className="container">
-        <form
-          id="contact"
-          action="https://usebasin.com/f/25163cad755a"
-          method="POST"
-        >
-          <h1>Contact me</h1>
-          <label htmlFor="Email">Name:</label>
-          <input
-            value={formData.name}
-            onChange={onFormChange}
-            type="text"
-            name="name"
-            id="name"
-          />
-          <label htmlFor="Email">Email:</label>
-          <input
-            value={formData.email}
-            onChange={onFormChange}
-            type="text"
-            name="email"
-            id="email"
-          />
-          <label htmlFor="message">Message:</label>
+      <form
+        id="contact"
+        action="https://usebasin.com/f/25163cad755a"
+        method="POST"
+      >
+        <h2 className="form__title">Send me a message</h2>
+        <p>Thank you for taking the time to write!</p>
+
+        <div className="form__top">
+          <div className="form__field">
+            <label htmlFor="Email">Name</label>
+            <input
+              value={formData.name}
+              onChange={onFormChange}
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Enter your name"
+            />
+          </div>
+          <div className="form__field">
+            <label htmlFor="Email">Email</label>
+            <input
+              value={formData.email}
+              onChange={onFormChange}
+              type="text"
+              name="email"
+              id="email"
+              placeholder="Enter your email address"
+            />
+          </div>
+        </div>
+        <div className="form__bottom">
+          <label htmlFor="message">Message</label>
           <textarea
             value={formData.message}
             onChange={onFormChange}
             name="message"
             id="message"
-          ></textarea>
-          <input className="btn submit" type="submit" value="Submit" />
-        </form>
-      </div>
+            placeholder="Hi, I would love to work with you."
+          />
+        </div>
+        <input className="btn submit" type="submit" value="Submit" />
+      </form>
     </section>
   );
 };
