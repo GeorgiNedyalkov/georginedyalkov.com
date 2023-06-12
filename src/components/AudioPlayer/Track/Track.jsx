3720cec808c9df4d19d8dd899974bcface283def
Flu3nt <gnedyalkov94@gmail.com>
2023-06-12 09:58:29 +0300

    feature: add a track component
    users can see my favorite tracks
    the artists, song and thumbnail
    users can play and listen to the songs

---
+++ src/components/AudioPlayer/Track/Track.jsx
@@ -0,0 +1,35 @@
+const Track = ({ track, audioRef, setDuration, progressBarRef }) => {
+  const onLoadedMetadata = () => {
+    const seconds = audioRef.current.duration;
+    setDuration(seconds);
+    progressBarRef.current.max = seconds;
+  };
+
+  return (
+    <article className="track">
+      <div className="track__top">
+        <div className="track__left">
+          <h3 className="track__artist">{track.artist}</h3>
+          <h5 className="track__title">{track.title}</h5>
+          <audio
+            src={track.src}
+            ref={audioRef}
+            onLoadedMetadata={onLoadedMetadata}
+          ></audio>
+        </div>
+        <div className="track__right">
+          <div className="track__img_container">
+            <img
+              className="track__thumbnail"
+              src={track.thumbnail}
+              alt={`A picture of ${track.artist}`}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="track__bottom"></div>
+    </article>
+  );
+};
+
+export default Track;
