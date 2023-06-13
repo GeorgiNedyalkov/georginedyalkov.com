facf3f16d172285ad70b00be4a903eae60501d31
Flu3nt <gnedyalkov94@gmail.com>
2023-06-13 15:39:37 +0300

    chore: add additional assets

---
+++ src/data/booksData.js
@@ -0,0 +1,62 @@
+import war_of_art from "../assets/images/war_of_art.jpg";
+import shogun from "../assets/images/shogun.jpg";
+import the_fountainhead from "../assets/images/the_fountainhead.jpg";
+import sherlock from "../assets/images/sherlock.jpg";
+
+export const booksData = [
+  {
+    id: 1,
+    title: "The War of Art",
+    author: "Stevem Pressfield",
+    cover: war_of_art,
+    description: `Since 2002, The War of Art has inspired people around the world to defeat "Resistance"; 
+    to recognize and knock down dream-blocking barriers and to silence the naysayers within us.Resistance kicks everyone's butt, 
+    and the desire to defeat it is equally as universal. The War of Art identifies the enemy that every one of us must face, 
+    outlines a battle plan to conquer this internal foe, then pinpoints just how to achieve the greatest success.Though it was written for writers, 
+    it has been embraced by business entrepreneurs, actors, dancers, painters, photographers, filmmakers, military service members and thousands of others around the world.`,
+    genre: "personal development",
+    link: "https://www.amazon.com/War-Art-Steven-Pressfield-ebook/dp/B007A4SDCG/ref=sr_1_1?keywords=war+of+art&s=digital-text&sr=1-1",
+    sample:
+      "https://www.amazon.com/War-Art-Steven-Pressfield-ebook/dp/B007A4SDCG?asin=B007A4SDCG&revisionId=7ab18374&format=1&depth=1",
+  },
+  {
+    id: 2,
+    title: "Shōgun",
+    author: "James Clavell",
+    cover: shogun,
+    description: `After Englishman John Blackthorne is lost at sea, he awakens in a place few Europeans know of and even fewer have seen—Nippon. 
+    Thrust into the closed society that is seventeenth-century Japan, a land where the line between life and death is razor-thin, 
+    Blackthorne must negotiate not only a foreign people, with unknown customs and language, but also his own definitions of morality, truth, and freedom. 
+    As internal political strife and a clash of cultures lead to seemingly inevitable conflict, Blackthorne’s loyalty and strength of 
+    character are tested by both passion and loss, and he is torn between two worlds that will each be forever changed.`,
+    genre: "fiction",
+    link: "https://www.amazon.com/Sh%C5%8Dgun-Asian-Saga-Book-1-ebook/dp/B0BVXSYMD8/ref=sr_1_1?keywords=shogun&s=digital-text&sr=1-1",
+    sample:
+      "https://www.amazon.com/Sh%C5%8Dgun-Asian-Saga-Book-1-ebook/dp/B007A4SDCG?asin=B007A4SDCG&revisionId=7ab18374&format=1&depth=1",
+  },
+  {
+    id: 3,
+    title: "The Fountainhead",
+    author: "Ayn Rand",
+    cover: the_fountainhead,
+    description: `The story of intransigent young architect Howard Roark, whose integrity was as unyielding as granite...of Dominique Francon, 
+    the exquisitely beautiful woman who loved Roark passionately, but married his worst enemy...and of the fanatic denunciation unleashed by an enraged society against a great creator.`,
+    genre: "fiction",
+    link: "https://www.amazon.com/Fountainhead-Ayn-Rand-ebook/dp/B002OSXDAU/ref=sr_1_1?keywords=The+Fountainhead&s=digital-text&sr=1-1",
+    sample:
+      "https://www.amazon.com/Fountainhead-Ayn-Rand-ebook/dp/B002OSXDAU/ref=sr_1_1?keywords=the+fountainhead&s=digital-text&sr=1-1&asin=B002OSXDAU&revisionId=d1f7adea&format=1&depth=1",
+  },
+  {
+    id: 4,
+    title: "Sherlock Homes",
+    author: "Arthur Conan Doyle",
+    cover: sherlock,
+    description: `Sherlock Holmes is a fictional detective of the late 19th and early 20th centuries, who first appeared in publication in 1887. 
+    He is the creation of Scottish born author and physician Sir Arthur Conan Doyle. A brilliant London-based detective, Holmes is famous for his intellectual prowess, 
+    and is renowned for his skillful use of deductive reasoning (somewhat mistakenly - see inductive reasoning) and astute observation to solve difficult cases. 
+    He is arguably the most famous fictional detective ever created, and is one of the best known and most universally recognizable literary characters in any genre.`,
+    genre: "fiction",
+    sample:
+      "https://www.amazon.com/Fountainhead-Ayn-Rand-ebook/dp/B002OSXDAU/ref=sr_1_1?keywords=the+fountainhead&s=digital-text&sr=1-1&asin=B002OSXDAU&revisionId=d1f7adea&format=1&depth=1",
+  },
+];
---
+++ src/data/moviesData.js
@@ -0,0 +1,12 @@
+export const moviesData = [
+  {
+    id: 1,
+    key: "Lord of the Rings",
+    query: "?t=The+Lord+of+the+Rings",
+  },
+  {
+    id: 2,
+    key: "Snatch",
+    query: "t=snatch",
+  },
+];
