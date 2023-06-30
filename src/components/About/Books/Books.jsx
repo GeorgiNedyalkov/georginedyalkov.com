ed9185b12b803fc6f4b32bd16dbada83074c9d59
Flu3nt <gnedyalkov94@gmail.com>
2023-06-30 16:40:08 +0300

    books: add responsive desing to books

--- src/components/About/Books/Books.jsx
+++ src/components/About/Books/Books.jsx
@@ -3,21 +3,23 @@ import Book from "./Book/Book";
 import "./Books.css";
 
 const Books = ({ books, onBookMouseOver, currentBookIndex }) => {
+  const bookList = (
+    <ul className="books__list">
+      {books.map((book) => {
+        return (
+          <li key={book.id} onMouseOver={() => onBookMouseOver(book.id)}>
+            <a className="link" href={book.link}>
+              {book.title}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+
   return (
     <div className="books__section">
-      <div className="books__section-left">
-        <ul className="books__list">
-          {books.map((book) => {
-            return (
-              <li key={book.id} onMouseOver={() => onBookMouseOver(book.id)}>
-                <a className="link" href={book.link}>
-                  {book.title}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      <div className="books__section-left">{bookList}</div>
       <div className="books__section-right">
         <Book {...books[currentBookIndex - 1]} />
       </div>
