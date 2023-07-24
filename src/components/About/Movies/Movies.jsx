723de4e1d2f2e9acf462fd7c1b13c1dd3d0b5a9d
Flu3nt <gnedyalkov94@gmail.com>
2023-07-24 16:05:18 +0300

    refactor: movies and books components contain headings and surrounding div

--- src/components/About/Movies/Movies.jsx
+++ src/components/About/Movies/Movies.jsx
@@ -4,22 +4,28 @@ import "./Movies.css";
 
 const Movies = ({ movies, currentMovieIndex, onMovieMouseOver }) => {
   return (
-    <div className="movies">
-      <div className="movies__section--left">
-        <Movie {...movies[currentMovieIndex - 1]} />
-      </div>
-      <div className="movies__section--right">
-        <ul className="movies__list">
-          {movies.map((movie) => {
-            return (
-              <li onMouseOver={() => onMovieMouseOver(movie.id)} key={movie.id}>
-                <a className="link" href={movie.trailer}>
-                  {movie.title}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
+    <div className="about__section">
+      <h3 className="about__heading">Movies and Shows</h3>
+      <div className="movies">
+        <div className="movies__section--left">
+          <Movie {...movies[currentMovieIndex - 1]} />
+        </div>
+        <div className="movies__section--right">
+          <ul className="movies__list">
+            {movies.map((movie) => {
+              return (
+                <li
+                  onMouseOver={() => onMovieMouseOver(movie.id)}
+                  key={movie.id}
+                >
+                  <a className="link" href={movie.trailer}>
+                    {movie.title}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
