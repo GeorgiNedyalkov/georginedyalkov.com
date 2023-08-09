d7999a0038b1b9c43cd71836f81e8a0146d0cf6b
Flu3nt <gnedyalkov94@gmail.com>
2023-08-09 14:00:09 +0300

    add new project Interior Customiser

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,32 +1,38 @@
 export const projectsData = [
-  {
-    id: 1,
-    name: "georginedyalkov.com",
-    // imageUrl: Nabr,
-    description: `The source code of this website. Developed with React JS.`,
-    date: 2023,
-    code: "https://github.com/GeorgiNedyalkov/georgi_nedyalkov",
-  },
-  {
-    id: 2,
-    name: "Art Hub",
-    // imageUrl: ArtHub,
-    description: `Create and share your artwork with others. 
+    {
+        id: 1,
+        name: "georginedyalkov.com",
+        description: `The source code of this website. Developed with React JS.`,
+        date: 2023,
+        code: "https://github.com/GeorgiNedyalkov/georgi_nedyalkov",
+    },
+    {
+        id: 2,
+        name: "Art Hub",
+        description: `Create and share your artwork with others. 
     Full stack CRUD application built with React and Express`,
-    date: 2023,
-    demo: "https://art-hub-client.vercel.app/",
-    code: "https://github.com/GeorgiNedyalkov/artHub",
-  },
-  {
-    id: 3,
-    name: "Real Estate Web Scraper",
-    // imageUrl: RealEstateWebScraper,
-    description:
-      "A web scraper and statistics analyser for real estate market prices.",
-    date: 2022,
-    demo: "/",
-    code: "https://github.com/GeorgiNedyalkov/real-estate-web-scraper",
-  },
+        date: 2023,
+        demo: "https://art-hub-client.vercel.app/",
+        code: "https://github.com/GeorgiNedyalkov/artHub",
+    },
+    {
+        id: 3,
+        name: "Real Estate Web Scraper",
+        description:
+            "A web scraper and statistics analyser for real estate market prices.",
+        date: 2022,
+        demo: "/",
+        code: "https://github.com/GeorgiNedyalkov/real-estate-web-scraper",
+    },
+    {
+        id: 4,
+        name: "Interior Customiser",
+        description:
+            "A website that helps home buyers customise their dream home.",
+        date: 2022,
+        demo: "/https://interior-customizer-qxyg.vercel.app/",
+        code: "https://github.com/GeorgiNedyalkov/interior-customizer/tree/main",
+    },
 ];
 
 // SOFTUNI PROJECTS
