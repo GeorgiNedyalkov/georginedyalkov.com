6577471e53e446d454188b39fc685e80ed48819e
Flu3nt <gnedyalkov94@gmail.com>
2023-08-28 10:51:44 +0300

    update my story

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -18,16 +18,21 @@ const Home = ({ projects }) => {
                 <div id="story">
                     <h2>What's the story?</h2>
                     <p>
-                        For the past 7 years I have been working in a real
+                        For the past 6 years I have been working in a real
                         estate development company called Atlantis Bulgaria
                         Holding.
                     </p>
                     <p>
-                        I have been learning how to code, for a little over a
-                        year now, by myself at first and then with SoftUni so
-                        that I can validate what I know with best practices. I
-                        have one more semester to complete the JavaScript
-                        developer path.
+                        For the past year and a half, I have been learning the
+                        fundamentals of web development, designing and building
+                        full stack applications that solve problems from my work
+                        experience as a real estate marketing and sales
+                        development. Part of my job was to create make a monthly
+                        market analysis report which was a manual, repetitive
+                        and time—consuming task. My first application did all
+                        the work manually and saved me lots of hours and pains,
+                        thus igniting my spark to create tools that help solve
+                        real world problems.
                     </p>
                 </div>
             </section>
